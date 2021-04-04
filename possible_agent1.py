1eb8cf665af153ca8f7802511280aa37864c9310
kajolbhat <40242157+kajolbhat@users.noreply.github.com>
2021-04-04 17:15:48 -0400

    Update possible_agent1.py

--- possible_agent1.py
+++ possible_agent1.py
@@ -8,7 +8,7 @@ import random
 from collections import deque
 import random
 
-n = 4
+n = 30
 colors = [mpl.cm.ocean(1),mpl.cm.binary(0),mpl.cm.binary(0.4),
               mpl.cm.binary(0.7)]
 cmap = m.colors.ListedColormap(colors)
@@ -56,7 +56,7 @@ def create():
                 newmatrix[i,j] = num_arr[l]
                 l+=1
 
-    print(newmatrix)
+    #print(newmatrix)
     return newmatrix
 
 newmatrix = create()
@@ -84,44 +84,91 @@ for i in range(n):
     #for j in range(n):
         #flat
 
-print(newmatrix) 
+#print(newmatrix) 
 
  
-
-        
-
-
+def ManhattanDistance(a1,b1,a2,b2):
+    total =  abs(a1 - a2) + abs(b1 - b2) 
+    #print("1= ", a1, b1, "2=", a2,b2, total)
+    return total
 ## if coordinate is visited equals 1 else equals 0
+visited = np.zeros((n,n))
+##Manhattan = 0
 
-def fncalc(i, newmatrix):
-    if (newmatrix[i] == 2):
-        #prob_matrix[i] *= .1
-        # changed to 1-p
-        prob_matrix[i] *= .9
-        print("flat,  = ", prob_matrix[i])
-        curr = prob_matrix[i]
-    #forest
-    elif (newmatrix[i] == 1):
-        #prob_matrix[i]  *= .7
-        # changed to 1-p
-        prob_matrix[i]  *= .3
-        print("firest,  = ", prob_matrix[i])
-        curr = prob_matrix[i]
-    #hilly 
-    elif (newmatrix[i] == 3):
-        #prob_matrix[i] *=  .3
+def calc_fn(newmatrix, Coord1, Coord2):
+    if (newmatrix[Coord1, Coord2] == 2):
         #changed to 1-p
-        prob_matrix[i]  *= .7
-        print("hill,  = ", prob_matrix[i])
-        curr = prob_matrix[i]
+        #prob_matrix[i] *= .1
+        prob_matrix[Coord1, Coord2] *= .9
+        #print("flat,  = ", prob_matrix[Coord1, Coord2])
+        curr = prob_matrix[Coord1, Coord2]
+        #tup_list.append(((a,b), curr, ManhattanDistance(a, b, Coord1, Coord2)))
+    #forest
+    elif (newmatrix[Coord1, Coord2] == 1):
+        #changed to 1-p
+        #prob_matrix[i]  *= .7
+        prob_matrix[Coord1, Coord2] *= .3
+        #print("firest,  = ", prob_matrix[Coord1, Coord2])
+        curr = prob_matrix[Coord1, Coord2]
+        #tup_list.append(((a,b), curr, ManhattanDistance(a, b, Coord1, Coord2)))
+    #hilly 
+    elif (newmatrix[Coord1, Coord2] == 3):
+        #changed to 1-p
+        #prob_matrix[i] *=  .3
+        prob_matrix[Coord1, Coord2] *= .7
+        #print("hill,  = ", prob_matrix[Coord1, Coord2])
+        curr = prob_matrix[Coord1, Coord2]
+        #tup_list.append(((a,b), curr, ManhattanDistance(a, b, Coord1, Coord2)))
     #cave 
     ## the zeros are a temp fix
-    elif (newmatrix[i] == 4):
+    elif (newmatrix[Coord1, Coord2] == 4):
+        #changed to 1-p
         #prob_matrix[i] *= .9
-        prob_matrix[i] *= .1
-        print("cave,  = ", prob_matrix[i])
-        curr = prob_matrix[i]
-    print("after method", prob_matrix)
+        prob_matrix[Coord1, Coord2] *= .1
+        #print("cave,  = ", prob_matrix[Coord1, Coord2])
+        curr = prob_matrix[Coord1, Coord2]
+        #tup_list.append(((a,b), curr, ManhattanDistance(a, b, Coord1, Coord2)))
+    #print("after method", prob_matrix)
+    
+manList = []
+def open_cell(newmatrofix, prob_matrix,Coord1,Coord2):
+    tup_list = [] 
+    
+    for a in range(n):
+        for b in range(n):
+            if visited[a,b] != 1:
+                tup_list.append(((a,b), prob_matrix[a,b], ManhattanDistance(a, b, Coord1, Coord2)))
+                tup_list.sort(key=lambda x: (-x[1], x[2]))
+            else:
+                pass
+                #print("already visited")
+    #tup_list.sort(key=lambda x: x[1], reverse = True)
+    
+
+    
+    
+    
+
+    
+    ## find maximum value in the mrix and open that 
+    i = tup_list[0][0]
+    ## calculate Manhattan
+    manList.append(ManhattanDistance(i[0], i[1], Coord1, Coord2))
+    #print("current value", i)
+    #j = tup_list[0][0]
+    
+
+
+   ## print(tup_list)
+    
+    visited[i] = 1
+    #print(visited)
+    #print("before method", prob_matrix)
+    #print(tup_list)
+    #print("this is new", newmatrix[i,j])
+    #print("original,  = ", prob_matrix[i])
+    #print("this is new matrix " , newmatrix[i])
+    
     
     tot = 0
     ## add everything together
@@ -135,60 +182,33 @@ def fncalc(i, newmatrix):
         for a in range(n):
             for b in range(n):
                 prob_matrix[a,b] = float(prob_matrix[a,b]) / float(tot)
-    #tup_list.clear()
-    
-    return newmatrix
-visited = np.zeros((n,n))
-def open_cell(newmatrofix, prob_matrix):
-    tup_list = [] 
-    print(tup_list)
-    for a in range(n):
-        for b in range(n):
-            if visited[a,b] != 1:
-                tup_list.append(((a,b), prob_matrix[a,b]))
-            else:
-                print("already visited")
-    tup_list.sort(key=lambda x: x[1], reverse = True)
-    
-    
-
-    
-    ## find maximum value in the mrix and open that 
-    i = tup_list[0][0]
-    print("current value", i)
-    #j = tup_list[0][0]
-    
-
-
-    print(tup_list)
-    
-    visited[i] = 1
-    #print(visited)
-    print("before method", prob_matrix)
+                
     #print(tup_list)
-    #print("this is new", newmatrix[i,j])
-    print("original,  = ", prob_matrix[i])
-    print("this is new matrix " , newmatrix[i])
-    
+    tup_list.clear()
     return i
 
 found = False
 x_val = 0
 y_val = 0
 #print("this is found", found)
+Coord1 = 0
+Coord2 = 0
 while found == False:  
-    x_val, y_val = open_cell(newmatrix, prob_matrix)
+    x_val, y_val = open_cell(newmatrix, prob_matrix,Coord1, Coord2)
     #open_cell(newmatrix, prob_matrix)
     ## if equals target
     if (x_val == x and y_val ==y):
         found = True
         print("true found")
     else:
-        newmatrix = fncalc((x_val, y_val), newmatrix)
+        Coord1 = x_val
+        Coord2 = y_val
+        #print("coord = ", Coord1, Coord2)
+        calc_fn(newmatrix, Coord1, Coord2)
         continue
    
     
-print(found, x_val, y_val)
+print(found, x_val, y_val, "Manhattan = " + str(sum(manList)))
 
        
 ## if target not found, normalize
