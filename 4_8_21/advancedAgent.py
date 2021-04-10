278fb97c2825d9433d4083ceee3d63104132b6d1
kajolbhat <40242157+kajolbhat@users.noreply.github.com>
2021-04-10 13:24:23 -0400

    Update advancedAgent.py

--- 4_8_21/advancedAgent.py
+++ 4_8_21/advancedAgent.py
@@ -9,14 +9,15 @@ import random
 from collections import deque
 import random
 
-n =30
+n =50
 colors = [mpl.cm.ocean(1),mpl.cm.binary(0),mpl.cm.binary(0.4),
               mpl.cm.binary(0.7)]
 cmap = m.colors.ListedColormap(colors)
 
 
+## Create matrix
 def create():
-    # create matrix
+    # create matrix of equal proportion of all terrains
     p = 0.25
     #start = (0,0)
     #goal = (n-1, n-1)
@@ -33,12 +34,11 @@ def create():
     newmatrix = np.zeros((n,n))
     numBlocks = math.ceil(n*n*p)
     numBlocks = int(numBlocks)
-    #print(numBlocks)
     matrix = np.zeros((n,n))
 
     cmap= m.colors.ListedColormap(colors)
 
-    ## 6,12,13,14
+
     num_arr = []
     for i in range(numBlocks):
         num_arr.append(1)
@@ -46,10 +46,10 @@ def create():
         num_arr.append(3)
         num_arr.append(4)
 
-    #print(num_arr)
-    random.shuffle(num_arr)
-    #print(num_arr)
 
+    random.shuffle(num_arr)
+
+    ## loading new_matrix, the matrix that contains all of the terrain types
     counter = 0
     l=0
     x = n*n
@@ -60,10 +60,11 @@ def create():
                 newmatrix[i,j] = num_arr[l]
                 l+=1
 
-    #print(newmatrix)
     return newmatrix
 
 newmatrix = create()
+
+## pick random target value
 x = np.random.randint(0,n-1)
 y = np.random.randint(0,n-1)
 
@@ -85,19 +86,16 @@ for i in range(n):
     ## 2 = white -> flat ====> .1
     ## 3 = gray -> hilly =====> .3
     ## 4 = dark gray -> cave ===> .9
-#for i in range(n):
-    #for j in range(n):
-        #flat
 
-#print(newmatrix) 
 
- 
+
+
+# calculate Manhattan Distance
 def ManhattanDistance(a1,b1,a2,b2):
     total =  abs(a1 - a2) + abs(b1 - b2) 
     #print("1= ", a1, b1, "2=", a2,b2, total)
     return total
-## if coordinate is visited equals 1 else equals 0
-##Manhattan = 0
+
 
 def neighbors(arr, d, coordinate):
     neighborList= []
@@ -128,17 +126,18 @@ def neighbors(arr, d, coordinate):
 
 
 
+### update the probability matrix using the false negative rates
+## value multiplied by (1-fnr)
 def calc_fn(newmatrix, Coord1, Coord2):
     if (newmatrix[Coord1, Coord2] == 2):
         #changed to 1-p
-        #prob_matrix[i] *= .1
+        #hilly
         prob_matrix[Coord1, Coord2] *= .9
         #print("flat,  = ", prob_matrix[Coord1, Coord2])
         curr = prob_matrix[Coord1, Coord2]
         #tup_list.append(((a,b), curr, ManhattanDistance(a, b, Coord1, Coord2)))
     #forest
     elif (newmatrix[Coord1, Coord2] == 1):
-        #changed to 1-p
         #prob_matrix[i]  *= .7
         prob_matrix[Coord1, Coord2] *= .3
         #print("firest,  = ", prob_matrix[Coord1, Coord2])
@@ -146,7 +145,6 @@ def calc_fn(newmatrix, Coord1, Coord2):
         #tup_list.append(((a,b), curr, ManhattanDistance(a, b, Coord1, Coord2)))
     #hilly 
     elif (newmatrix[Coord1, Coord2] == 3):
-        #changed to 1-p
         #prob_matrix[i] *=  .3
         prob_matrix[Coord1, Coord2] *= .7
         #print("hill,  = ", prob_matrix[Coord1, Coord2])
@@ -155,7 +153,6 @@ def calc_fn(newmatrix, Coord1, Coord2):
     #cave 
     ## the zeros are a temp fix
     elif (newmatrix[Coord1, Coord2] == 4):
-        #changed to 1-p
         #prob_matrix[i] *= .9
         prob_matrix[Coord1, Coord2] *= .1
         #print("cave,  = ", prob_matrix[Coord1, Coord2])
@@ -242,9 +239,12 @@ while found == False:
     false_neg[3] = .3
     false_neg[4] = .9
     
+    ## chance picks a random decimal value between 0 and 1 -> if greater than false negative rate, then the agent actually finds the target when looking in the correct target cell
+    ## otherwise will be a false negative
     chance = random.uniform(0,1)
     terrain = newmatrix[x_val, y_val]
     
+    ## if cave or forest we check twice
     if (terrain == .7) or (terrain == .9):
         if (x_val == x and y_val ==y):
             if (chance > false_neg[terrain]):
@@ -263,6 +263,7 @@ while found == False:
             found = True
             print("true found")
     else:
+      ## set original value looked at to Coord1 and 2 and then continue to find next value
         Coord1 = x_val
         Coord2 = y_val
         #print("coord = ", Coord1, Coord2)
